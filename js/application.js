f8140388e2285c57f03d22dd0fbfc9f04f0c8f48
Ethan <blue2095@gmail.com>
2017-01-05 12:36:45 -0500

    Core

--- js/application.js
+++ js/application.js
@@ -1,4 +1,4 @@
-// Wait till the browser is ready to render the game (avoids glitches)
+// Wait until the browser is ready to render the game (to avoid glitches)
 window.requestAnimationFrame(function () {
   new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
 });
